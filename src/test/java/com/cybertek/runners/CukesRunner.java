5b4821378ed2f15b80b59d5560e8e012633b764d
mariamrashad82 <mariam.rashad@yahoo.com>
2021-07-23 22:13:58 -0400

    Add more classes

--- src/test/java/com/cybertek/runners/CukesRunner.java
+++ src/test/java/com/cybertek/runners/CukesRunner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/cybertek/step_definitions",//execute Hook annotation
         dryRun = false,
-        tags ="@scenarioOutline"
+        tags ="@wip"
 
 )
 public class CukesRunner {
